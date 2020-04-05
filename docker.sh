de3266daf82ad42e44b25e4d7349e82f6b1641db
Cory Metcalfe <corymetcalfe@gmail.com>
2020-04-04 20:26:45 -0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,12 @@
 curl -fsSL https://get.docker.com -o get-docker.sh
-$ sudo sh get-docker.sh
+sudo sh get-docker.sh
+rm get-docker.sh
+
+# https://docs.docker.com/compose/install/
+
+# Install required packages
+sudo apt install -y python3-pip libffi-dev
+
+# Install Docker Compose from pip (using Python3)
+# This might take a while
+sudo pip3 install docker-compose
